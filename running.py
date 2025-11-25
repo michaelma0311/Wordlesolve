ba2252f6edc9577b0cdba17f8e3a051ae53398fb
Michael <michaelma0311@gmail.com>
2025-11-25 15:54:33 -0800

    Adding files for reployment

--- running.py
+++ running.py
@@ -152,7 +152,6 @@ def reset_form():
     lis = [] 
     return '<h1>Form reset successful, please go back to the previous page'
 if __name__ == '__main__':
-    app.run(debug = False)
-
+    app.run(host="0.0.0.0", port=5000)
 
 
